4ecdf13a609a8de0e5832837037fde686a7ef237
dynamic.malay@gmail.com <dynamic.malay@gmail.com>
2021-02-13 22:37:42 +0530

    v1 final 2

--- src/Drawer.js
+++ src/Drawer.js
@@ -102,7 +102,6 @@ export default function SideBar() {
         <main className={classes.content}>
           <div className={classes.toolbar} />
           <Switch>
-            <IndexRoute component={AllCustomers} />
             <Route path="/customers" component={AllCustomers}></Route>
             <Route path="/applications" component={LoanApplications}></Route>
           </Switch>
